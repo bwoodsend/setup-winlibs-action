3c97962457873b5fb5c8fa8d627139d8916ff23c
bwoodsend <bwoodsend@gmail.com>
2021-06-23 12:15:27 +0100

    Test compiling a hello world script.

--- validate.py
+++ validate.py
@@ -7,9 +7,11 @@ import os
 import unittest
 import sys
 import ast
+import re
+from subprocess import run, PIPE
 
 tc = unittest.TestCase()
-bin, in_path, with_clang = sys.argv[1:]
+bin, in_path, with_clang, architecture = sys.argv[1:]
 
 in_path = ast.literal_eval(in_path)
 with_clang = ast.literal_eval(with_clang)
@@ -32,3 +34,30 @@ if with_clang:
         tc.assertEqual(shutil.which("clang").lower(), clang.lower())
 else:
     assert not os.path.exists(clang), clang
+
+C_CODE = """
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    printf("Hello World!\\n");
+    printf("Size of pointer is %i.\\n", sizeof(size_t));
+    return 0;
+}
+"""
+
+def test_compile(cc):
+    exe = "./test-executable"
+    run([cc, "-o", exe, "-x", "c", "-"],
+        input=C_CODE, universal_newlines=True, check=True)
+    p = run([exe], check=True, stdout=PIPE, universal_newlines=True)
+    tc.assertRegex(p.stdout, r"Hello World!\nSize of pointer is \d\.\n")
+
+    tc.assertEqual(
+        re.search(r"Size of pointer is (\d)", p.stdout).group(1),
+        "8" if architecture == "x86_64" else "4",
+    )
+
+test_compile(gcc)
+if with_clang:
+    test_compile(clang)
