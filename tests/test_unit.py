e0668e59a98541e2b24ee8e49eff1880b9376dd6
Brénainn Woodsend <bwoodsend@gmail.com>
2022-06-06 21:13:24 +0100

    Rename select_version.py to install.py.

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -8,21 +8,21 @@ ROOT = HERE.parent
 # Ensure that scripts in the root of this repo are importable.
 sys.path.insert(0, str(ROOT))
 
-import select_version
+import install
 
 
 def test_normalise_architecture():
     """Test converting user input architectures to internal ones."""
 
-    assert select_version.normalise_architecture("x86_64") == "x86_64"
-    assert select_version.normalise_architecture("i686") == "i686"
-    assert select_version.normalise_architecture(32) == "i686"
-    assert select_version.normalise_architecture("64") == "x86_64"
+    assert install.normalise_architecture("x86_64") == "x86_64"
+    assert install.normalise_architecture("i686") == "i686"
+    assert install.normalise_architecture(32) == "i686"
+    assert install.normalise_architecture("64") == "x86_64"
 
     with pytest.raises(ValueError, match=_error_pattern):
-        select_version.normalise_architecture("62")
+        install.normalise_architecture("62")
     with pytest.raises(ValueError, match=r" 99\."):
-        select_version.normalise_architecture(99)
+        install.normalise_architecture(99)
 
 
 _error_pattern = r"""
@@ -37,4 +37,4 @@ def test_paginated_releases():
     """Test the paginated release searching will still work when the number of
     WinLibs releases available exceeds the page size.
     """
-    assert len(select_version.releases(25)) > 30
+    assert len(install.releases(25)) > 30
