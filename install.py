d53680fb6db485dd2711886273b06f66e91143b3
Brénainn Woodsend <bwoodsend@gmail.com>
2023-01-17 23:27:54 +0000

    Update default WinLibs tag to 12.2.0-15.0.7-10.0.0-msvcrt-r4.

--- install.py
+++ install.py
@@ -16,7 +16,7 @@ import argparse
 # release with the latest version of gcc.
 # I'm hard coding this tag in for now. Maybe I'll automate this if it
 # seems like a good idea in the future.
-LATEST = "12.2.0-15.0.6-10.0.0-msvcrt-r3"
+LATEST = "12.2.0-15.0.7-10.0.0-msvcrt-r4"
 
 Asset = namedtuple("ReleaseAsset", ["name", "url"])
 
